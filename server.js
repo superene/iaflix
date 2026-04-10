c0a10d9146df58d18759f05e3ed662e4b18319f0
Rebe <thelegacycontinua@gmail.com>
2026-04-10 13:25:36 -0600

    fix admin login

--- server.js
+++ server.js
@@ -98,7 +98,24 @@ app.post("/register", async (req, res) => {
     res.status(500).json({ mensaje: "Error servidor" })
   }
 })
+/* =========================
+   🔐 LOGIN ADMIN (FIX)
+========================= */
+app.post("/login", (req, res) => {
+  const { username, password } = req.body
 
+  if (username === "admin" && password === "1234") {
+    const token = jwt.sign(
+      { role: "admin" },
+      SECRET,
+      { expiresIn: "2h" }
+    )
+
+    return res.json({ token })
+  }
+
+  res.status(401).json({ mensaje: "Credenciales incorrectas" })
+})
 /* =========================
    🔐 LOGIN USER
 ========================= */
